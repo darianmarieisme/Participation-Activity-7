950786c1f3efac1f2ee378390229a307cfb6a6c3
Darian Bruce <darianmariebruce@gmail.com>
2026-02-25 20:30:01 -0500

    while loop logic

--- Participation-Activity-7.py
+++ Participation-Activity-7.py
@@ -2,3 +2,17 @@
 Darian Marie Bruce
 This program takes user input for pizza toppings
 02/25/2026'''
+
+ask_for_toppings: bool = True
+
+print('To exit the program at any time type "q" into the console.')
+
+while ask_for_toppings:
+    toppings_input: str = input("Please type the toppings you want on your pizza: ")
+    if toppings_input.strip().lower() == 'q':
+        ask_for_toppings = False
+    else:
+        print(f"You have typed in {toppings_input}. Your pizza will have {toppings_input} on it.")
+
+
+print('The program has ended.')
