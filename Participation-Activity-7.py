9f2b0399f9f953a95d591b95df52965a0bba6377
Darian Bruce <darianmariebruce@gmail.com>
2026-02-25 20:39:40 -0500

    finished program

--- Participation-Activity-7.py
+++ Participation-Activity-7.py
@@ -9,7 +9,10 @@ print('To exit the program at any time type "q" into the console.')
 
 while ask_for_toppings:
     toppings_input: str = input("Please type the toppings you want on your pizza: ")
-    if toppings_input.strip().lower() == 'q':
+    if not isinstance(toppings_input, str):
+        print("Please enter a valid topping.")
+        continue
+    elif toppings_input.strip().lower() == 'q':
         ask_for_toppings = False
     else:
         print(f"You have typed in {toppings_input}. Your pizza will have {toppings_input} on it.")
